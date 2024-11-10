f9f9dab5277d1e9ed2a74190869600a044db1391
takanashisatou <156740808+takanashisatou@users.noreply.github.com>
2024-11-10 21:37:13 +0800

    正在编写电弧发生器中

--- .minecraft/kubejs/startup_scripts/ArcGenerator.js
+++ .minecraft/kubejs/startup_scripts/ArcGenerator.js
@@ -1,5 +1,5 @@
 GTCEuStartupEvents.registry('gtceu:machine', event => {
-    event.create('small_greenhouses', 'simple', GTValues.IV)//名字和配方类型相同、单方块、机器电压
-        .recipeType('small_greenhouses', true, true)//显示的配方类型名字
-        .workableTieredHullRenderer('gtceu:block/machines/smallgreenhouses')//设置机器材质
-})   }）
+    event.create('arc_generator', 'simple', GTValues.IV)//名字和配方类型相同、单方块、机器电压
+        .recipeType('arc_generator', true, true)//显示的配方类型名字
+        .workableTieredHullRenderer('gtlcecore:block/arcgenerator')//设置机器材质
+})
